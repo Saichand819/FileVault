b4dbf1b0e903baf1a24e40ce549891f7be814479
Saichand819 <saichandgupta819@gmail.com>
2025-02-02 22:24:49 +0530

    Initial Commit

---
+++ file-vault-service/src/main/java/com/example/file_vault_service/Application.java
@@ -0,0 +1,13 @@
+package com.example.file_vault_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileVaultAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileVaultAppApplication.class, args);
+	}
+
+}
---
+++ file-vault-service/src/test/java/com/example/file_vault_app/FileVaultAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_vault_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileVaultAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
